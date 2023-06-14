678fe26cae397e2474d1cc5ba5352bbc1a077dd0
Lexey Khom <92052116+LexeyKhom@users.noreply.github.com>
2023-06-14 14:37:11 +0300

    feat(6-spread): add iterable object

--- JavaScript/6-spread.js
+++ JavaScript/6-spread.js
@@ -1,3 +1,5 @@
+'use strict';
+
 /*
   'Spread operator' and 'Destructuring assignment' do not work.
   But you can do something similar using a synchronous Iterable,
@@ -9,3 +11,24 @@
   возвращающий Promise в `value`. При этом, `done` должен быть
   синхронным, иначе итерация никогда не закончится.
 */
+
+const iterable = {
+  [Symbol.iterator]() {
+    let i = 0;
+    const iterator = {
+      next() {
+        const res = i;
+        return {
+          value: new Promise((resolve) => {
+            setTimeout(() => {
+              resolve(res);
+            }, 500);
+          }),
+          done: ++i > 3,
+        };
+      },
+    };
+    return iterator;
+  },
+};
+
