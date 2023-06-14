7c3dea9a887470fd174c2a5139b01d60dff8740f
Lexey Khom <92052116+LexeyKhom@users.noreply.github.com>
2023-06-14 14:36:03 +0300

    docs(6-spread): add comment

---
+++ JavaScript/6-spread.js
@@ -0,0 +1,11 @@
+/*
+  'Spread operator' and 'Destructuring assignment' do not work.
+  But you can do something similar using a synchronous Iterable,
+  returning a Promise in `value`. However, `done` must be
+  synchronous, otherwise the iteration will never end.
+
+  'Spread оператор' и 'Деструктурирующее присваивание' не работают.
+  Но можно сделать что-то похожее, используя синхнонный Iterable,
+  возвращающий Promise в `value`. При этом, `done` должен быть
+  синхронным, иначе итерация никогда не закончится.
+*/
